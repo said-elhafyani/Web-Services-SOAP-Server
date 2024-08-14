4c683cfd95866a6496fe740bb5b3e30c15824c78
El-Said <said.elhafyani@usmba.ac.ma>
2024-08-14 19:35:39 +0100

    first commit

---
+++ src/main/java/ServerJW.java
@@ -0,0 +1,11 @@
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
+
+public class ServerJW {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+        System.out.println("Web service deploye sur http://0.0.0.0:9191/");
+
+    }
+}
+
